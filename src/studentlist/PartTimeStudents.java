b18f4356a75fff35b67a4dc98e5ef88921b4ca86
Harmanpreet Kaur <kau17997@sheridancollege.ca>
2025-01-22 14:59:15 -0500

    changed new part time student class

---
+++ src/studentlist/PartTimeStudents.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author harma
+ */
+public class PartTimeStudents extends Student {
+    private int address;
+    
+    public int getAddress(){
+        return address;
+    }
+    
+}
